9531aa1168206f5a27701ba3a8ad9a5df2b2bea0
Enrique Devars <edevars@localhost.localdomain>
2018-09-29 00:11:28 -0500

    adding a video

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react'
-import VideoPlayerLayout from '../components/VideoPlayerLayout';
-
+import VideoPlayerLayout from '../components/VideoPlayerLayout.js';
+import Video from '../components/video';
+import Title from '../components/title'
 class VideoPlayer extends Component {
-    render()
-    {
+    render(){
+    return(
         <VideoPlayerLayout>
-        
+           <Title
+                title='Es un video chido'
+           />
+           <Video
+                autoplay={true}
+                src='http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'
+           />
         </VideoPlayerLayout>
+        )
     }
 }
 
